4c94a85da1a8ee79015c5576c8ee40dba74a8cbc
kubo-ryunosuke <kubo.ryunosuke@c-tech-studio.com>
2025-11-21 19:31:49 +0900

    オニオンアーキテクチャと軽量DDDの適用、Employeeエンティティを追加

---
+++ backend/src/main/java/com/example/backend/domain/Employee.java
@@ -0,0 +1,22 @@
+package com.example.backend.domain;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private LocalDate joinDate;
+    private Integer departmentId;
+    private Integer baseAllowance;
+}
--- backend/src/main/java/com/example/backend/presentation/HelloController.java
+++ backend/src/main/java/com/example/backend/presentation/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.backend;
+package com.example.backend.presentation;
 
 import org.springframework.web.bind.annotation.RestController;
 
