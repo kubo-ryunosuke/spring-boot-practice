f9494c136aa752f80355c29c95426876fff162d4
kubo-ryunosuke <kubo.ryunosuke@c-tech-studio.com>
2025-11-26 13:46:34 +0900

    ロギングAspectクラス追加

---
+++ backend/src/main/java/com/example/backend/infrastructure/aop/LoggingAspect.java
@@ -0,0 +1,18 @@
+package com.example.backend.infrastructure.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    @Before("execution(* com.example..*Controller.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        
+        System.out.println("====== [AOP Log] " + className + "." + methodName + "() が呼ばれました ======");
+    }
+}
